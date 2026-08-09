0b857ec49a3a9e9c86a0e367990d3ae5a7197677
habibullah0404 <habibasseghaf@gmail.com>
2026-08-09 13:55:04 +0700

    Update page.tsx

--- app/edit-profil/page.tsx
+++ app/edit-profil/page.tsx
@@ -42,8 +42,7 @@ const createImage = (url: string): Promise<HTMLImageElement> =>
   new Promise((resolve, reject) => {
     const image = new Image();
     image.addEventListener('load', () => resolve(image));
-    image.addEventListener('error', (error) => reject(error));
-    image.setAttribute('crossOrigin', 'anonymous');
+    image.addEventListener('error', (error) => reject(error);
     image.src = url;
   });
 
